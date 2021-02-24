f02480b4d2e61a0e4a14ad964b072c3b615b7c0d
Zagg2732 <zagg2732@gmail.com>
2021-02-24 18:48:45 +0900

    repush

--- src/Project/BitEats.java
+++ src/Project/BitEats.java
@@ -1,6 +1,9 @@
 package Project;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class BitEats {
     ArrayList<Customer> customerList;
@@ -12,11 +15,67 @@ public class BitEats {
     }
 
     public void join() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("비트이츠 회원가입 시스템입니다!!");
+
+        System.out.println("아이디를 입력해주세요");
+        String id = scanner.nextLine();
+        System.out.println("비밀번호를 입력해주세요");
+        String password = scanner.nextLine();
 
     }
 
     public void login() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("비트이츠 로그인 시스템입니다!!");
 
+
+
+        System.out.println("아이디를 입력해주세요");
+        String id = scanner.nextLine();
+        System.out.println("비밀번호를 입력해주세요");
+        String password = scanner.nextLine();
+
+        String filename = "C:\\BitEats\\LoginInfo\\" + id + ".txt";
+
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        ObjectInputStream in = null;
+
+        try {
+            fis = new FileInputStream(filename); //파일이름불러오고
+            bis = new BufferedInputStream(fis); //불러온걸 버퍼로옮기고
+            in = new ObjectInputStream(bis); //버퍼로 옮긴걸 조립한다
+
+//          UserInfo r1 = (UserInfo) in.readObject(); //다운캐스팅을 해줘야함
+
+            LoginInfo loginInfo = (LoginInfo) in.readObject();
+            HashMap map = loginInfo.getLogin();
+
+            if (map.get(id).equals(password)) { //아이디와 키값이 같다면
+                System.out.println("로그인성공 ㅎ");
+            } else {
+                System.out.println("비밀번호가 틀립니다!!");
+            }
+
+        } catch (FileNotFoundException fn) {
+            System.out.println("해당 아이디가 존재하지 않습니다");
+        } catch (EOFException eofe) {
+            System.out.println("파일이 끝났어오");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } finally {
+            try {
+                in.close();
+                bis.close();
+                fis.close();
+            } catch (Exception e3) {
+
+            }
+        }
     }
 
     @Override
---
+++ src/Project/LoginInfo.java
@@ -0,0 +1,53 @@
+package Project;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class LoginInfo implements Serializable {
+    private HashMap<String, String> login ;
+    private static final long serialVersionUID = 3L;
+
+    LoginInfo(HashMap<String, String> hash) {
+        this.login = hash;
+    }
+
+    public HashMap<String, String> getLogin() {
+        return login;
+    }
+
+    public LoginInfo(String type, String id, HashMap<String, String> login) {
+        String path = "";
+
+        String filename = "C:\\BitEats\\LoginInfo\\" + path +"\\" +id + ".txt"; // 객체를 직렬화해서 write
+
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        ObjectOutputStream out = null;
+
+        try {
+            fos = new FileOutputStream(filename); //append
+            bos = new BufferedOutputStream(fos);
+            // 여기까지 하고 직렬화를 하기 위해 아래와 같이 한다
+            out = new ObjectOutputStream(bos);
+
+            //직렬화 대상 (객체)
+            LoginInfo loginInfo = new LoginInfo(login);
+
+            //이제 다른곳으로 보낼거임. filename으로
+            out.writeObject(loginInfo); // 분해해서 Userdata.txt 에 쓴다
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                out.close();
+                bos.close();
+                fos.close();
+            } catch (Exception e2) {
+
+            }
+        }
+    }
+}
+
